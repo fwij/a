2daf7ca36d19c23a14e6e68d3cce4821400a508f
fwij <87453177+fwij@users.noreply.github.com>
2023-03-09 06:33:29 -0500

    Add files via upload

---
+++ static/data/utilities.js
@@ -0,0 +1,27 @@
+var utilities = [
+  {
+    "id": "aboutblank",
+    "title": "about:blank launcher",
+    "url": "/utils/aboutblank.html"
+  },
+  {
+    'id': 'bookmarklets',
+    'title': 'Bookmarklets',
+    'url': '/utils/bookmarklets.html'
+  },
+  {
+    'id': 'win11',
+    'title': 'Windows 11 in React',
+    'url': '/load.html?url=https://win11.blueedge.me/'
+  },
+  {
+    'id': 'gbaemulator',
+    'title': 'GBA Emulator',
+    'url': '/load.html?url=https://cattn.github.io/gba'
+  },
+  {
+    'id': 'widgetbot',
+    'title': 'Widgetbot (our Discord server)',
+    'url': '/utils/widgetbot.html'
+  }
+]
